3717d821cad6bfe9832f8e4b422967a37d7263df
octoccoper <octocopper@gmail.com>
2019-01-23 19:28:07 +0200

    updated app.js

--- app.js
+++ app.js
@@ -17,15 +17,33 @@ function validateName() {
 
 function validateZip() {
   const zip = document.getElementById('zip');
- 
+  const re = /^[0-9]{5}(-[0-9]{4})$/;
+
+  if (!re.test(zip.value)) {
+    zip.classList.add("is-invalid");
+  } else { 
+    zip.classList.remove("is-invalid");
+  }
 }
 
 function validateEmail() {
   const email = document.getElementById('email');
- 
+  const re = /^[A-Za-z]{2,10}$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else { 
+    email.classList.remove("is-invalid");
+  }
 }
 
 function validatePhone() {
   const phone = document.getElementById('phone');
- 
+  const re = /^[A-Za-z]{2,10}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else { 
+    phone.classList.remove("is-invalid");
+  }
 }
